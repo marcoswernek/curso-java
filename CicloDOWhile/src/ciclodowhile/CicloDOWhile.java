7038ff89d98f3391622154e9033f7ed9c3512690
marcos <105993860+marcoswernek@users.noreply.github.com>
2022-07-23 12:17:28 -0400

    aulas de java...

---
+++ CicloDOWhile/src/ciclodowhile/CicloDOWhile.java
@@ -0,0 +1,17 @@
+
+package ciclodowhile;
+
+
+public class CicloDOWhile {
+
+    
+    public static void main(String[] args) {
+     int i = 0;
+     
+     do{
+         System.out.println("El numero es:"+i);
+         i++;
+     }while(i <= 10);
+    }
+    
+}
---
+++ CicloWhile/src/ciclowhile/CicloWhile.java
@@ -0,0 +1,17 @@
+
+package ciclowhile;
+
+
+public class CicloWhile {
+ //ciclo while
+    
+    public static void main(String[] args) {
+       int x = 1;
+       
+       while(x <= 5){
+           System.out.println("El valor de x es:"+x);
+           x++;
+       }
+    }
+    
+}
