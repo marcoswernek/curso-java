9a8b10ed4c902a10e31e67dc809e2fdd884dd3a8
marcos <105993860+marcoswernek@users.noreply.github.com>
2022-08-03 23:28:14 -0400

    aulas d java

---
+++ PaquetesyApis/src/paquetesyapis/PaquetesyApis.java
@@ -0,0 +1,11 @@
+
+package paquetesyapis;
+
+public class PaquetesyApis {
+
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+        System.out.println(Math.max(200, 1000));
+    }
+    
+}
